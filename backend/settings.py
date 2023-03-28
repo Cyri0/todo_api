b33c28901e6e88437b3d45e64f7d0251e5ceac37
Cyri0 <nemestamas94@gmail.com>
2023-03-28 18:49:28 +0200

    second commit

--- backend/settings.py
+++ backend/settings.py
@@ -16,6 +16,7 @@ from secret import key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#MÓDOSÍTÁS
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
